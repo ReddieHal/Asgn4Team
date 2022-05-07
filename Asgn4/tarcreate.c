7ace4db6939a42f1eef979cd32aba85a3bd88739
Gabriel Rodriguez <grodri87@unix2.csc.calpoly.edu>
2022-05-07 16:17:36 -0700

    bruh

--- Asgn4/tarcreate.c
+++ Asgn4/tarcreate.c
@@ -1,3 +1,10 @@
 void tarcreate(int file, char *path, bool verbose, bool strict) {
+    /*start at the path and read dir*/
+
+
+    /*is it directory*/
+    /*if it is change directory*/
+    /*get directory lstat info*/
+    /*add it to .tar */
 
 }
