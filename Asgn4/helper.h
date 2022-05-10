54db5aafbcc22f43ee3fe0dd47927c92d9df66ba
Gabriel Rodriguez <grodri87@unix2.csc.calpoly.edu>
2022-05-09 22:26:39 -0700

    added some define

--- Asgn4/helper.h
+++ Asgn4/helper.h
@@ -3,11 +3,15 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <sys/types.h>
-       #include <sys/stat.h>
-       #include <fcntl.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 #define BLOCKSIZE 512
-
+#define REGFILE '0'
+#define ALTFILE '\0'
+#define SYMLINK '2'
+#define DIRECT '5'
 typedef enum { false, true } bool;
 
 int checksum(char *);
--- Asgn4/tarextract.c
+++ Asgn4/tarextract.c
@@ -1,8 +1,47 @@
 #include "helper.h"
+#define SEGSIZE 4096
+#define HEADSIZE 512
 
 void tarextract(int file, char *path, bool verbose, bool strict) {
+    char buf[SEGSIZE];
+    char checksum[8];
+    char flag;
+    int charCount, i, sum, out;
     /*read the header */
     printf("running tarextract\n");
+
+    if ((charCount = read(file, &buf, HEADSIZE)) == -1) {
+        perror("read");
+        exit(1);
+    }
+    /*check check sum*/
+    for (i = 0; i < 8; i++) {
+        checksum[i] = buf[148 + i];
+        buf[148 + i] = ' ';
+    }
+    
+    sum = 0;
+    for (i = 0; i < HEADSIZE; i++) {
+        sum += (unsigned char)buf[i];
+    }
+
+    out = strtol(checksum, NULL, 8);
+
+    if (out != sum) {
+        fprintf(stderr, "Checksum doesn't match\n");
+    } 
+
+    flag = buf[156];
+
+    switch(flag) {
+        case DIRECT:
+            printf("dire\n");
+            break;
+        case SYMLINK:
+            break;
+        default :
+            break;
+    }
     /*check the flag type*/
     /*if its a directory*/
     /*  make the directory */
